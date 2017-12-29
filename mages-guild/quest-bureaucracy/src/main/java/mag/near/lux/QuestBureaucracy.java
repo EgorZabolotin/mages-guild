362eb13886555ae751cfe1b9d8609fae0a224861
EgorZabolotin <zes-87@yandex.ru>
2017-12-29 17:53:17 +0600

    Add encoding to unmarshaller for crimesList.

--- mages-guild/quest-bureaucracy/src/main/java/mag/near/lux/QuestBureaucracy.java
+++ mages-guild/quest-bureaucracy/src/main/java/mag/near/lux/QuestBureaucracy.java
@@ -2,18 +2,18 @@ package mag.near.lux;
 
 import mag.near.lux.dto.ArrayOfCrimeDTO;
 import mag.near.lux.dto.PersonDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
 import java.io.ByteArrayInputStream;
-import java.util.List;
+import java.nio.charset.StandardCharsets;
 import java.util.stream.Stream;
 
 public class QuestBureaucracy {
@@ -43,13 +43,14 @@ public class QuestBureaucracy {
                 JAXBContext jaxbContext = JAXBContext.newInstance(ArrayOfCrimeDTO.class);
                 Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
                 ArrayOfCrimeDTO arrayOfCrimeDTO = (ArrayOfCrimeDTO) unmarshaller.unmarshal(
-                        new StreamSource(new ByteArrayInputStream(crimes.getBody().getBytes())));
+                        new StreamSource(new ByteArrayInputStream(crimes.getBody().getBytes(StandardCharsets.ISO_8859_1))));
                 arrayOfCrimeDTO.getCrimes().stream()
                         .forEach(crime -> LOGGER.debug(crime.toString()));
             } catch (JAXBException e) {
                 LOGGER.error(e.toString());
             }
-        }else{
+        }
+        else{
             LOGGER.error("Error occurred while getting crimes. Response code is {}", crimes.getStatusCode());
         }
 
