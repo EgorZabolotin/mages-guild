59cab94da5287402f3e333896f87680b3cd7bfb9
EgorZabolotin <zes-87@yandex.ru>
2018-01-10 15:16:21 +0600

    Refactor:: add resource names as constants

--- mages-guild/quest-bureaucracy/src/main/java/mag/near/lux/services/MagesService.java
+++ mages-guild/quest-bureaucracy/src/main/java/mag/near/lux/services/MagesService.java
@@ -3,6 +3,7 @@ package mag.near.lux.services;
 import mag.near.lux.dto.PersonDTO;
 import mag.near.lux.model.MagePerson;
 import mag.near.lux.util.PropsUtil;
+import mag.near.lux.util.ResourcesNames;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -22,8 +23,8 @@ public class MagesService {
         RestTemplate restTemplate = new RestTemplate();
         PersonDTO[]  magesArray;
         List<PersonDTO> magesDTO = null;
-        PropsUtil propsUtil = new PropsUtil("endPoints.properties");
-        String getMagesUrl = propsUtil.getPropertyByNmae("get.mages.url");
+        PropsUtil propsUtil = new PropsUtil(ResourcesNames.END_POINTS);
+        String getMagesUrl = propsUtil.getPropertyByNmae(ResourcesNames.GET_MAGES_URL);
 
         ResponseEntity<PersonDTO[]> mages = restTemplate.getForEntity(getMagesUrl, PersonDTO[].class, limit.toString());
 
